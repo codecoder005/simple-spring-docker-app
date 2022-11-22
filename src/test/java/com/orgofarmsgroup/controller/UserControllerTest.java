844826c0dace9adaaf6a1df556f405dacf8667fa
Ramakrishna Janapureddy <85489242+codecoder005@users.noreply.github.com>
2022-11-22 16:43:10 +0530

    unit test added for usercontroller

---
+++ src/test/java/com/orgofarmsgroup/controller/UserControllerTest.java
@@ -0,0 +1,30 @@
+package com.orgofarmsgroup.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("users root api")
+    void testUsers() throws Exception {
+        mockMvc.perform(
+                get("/users")
+        ).andDo(print()).andExpect(status().isOk());
+    }
+}
